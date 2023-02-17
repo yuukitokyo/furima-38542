f25a75196d3e38aabb2d47b6725a2e4f7d0c915d
yuukitokyo <yamadayuuki2678@gmail.com>
2023-02-17 20:24:47 +0900

    モデルの単体テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {'1a' + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     family_name           {'山田'}
     first_name            {'太郎'}
