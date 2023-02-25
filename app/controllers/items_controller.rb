48344ebc9d9a276f7c07cda4408e6483aa23aa67
yuukitokyo <yamadayuuki2678@gmail.com>
2023-02-25 11:57:08 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,15 @@ def update
   end
 end
 
+def destroy
+    item = Item.find(params[:id])
+    if item.user_id == current_user.id
+    item.destroy 
+    redirect_to root_path
+    end
+
+end
+
 private
 def item_params
    params.require(:item).permit(:image, :item_name, :item_description, :item_category_id, :item_state_id, :delivery_burden_id, :delivery_area_id, :delivery_day_id, :item_price).merge(user_id: current_user.id)
